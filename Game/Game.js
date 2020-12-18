17a1a0dbc77b540eda0376e10bf1ea0cab5222f7
ilker <lkrbalci@gmail.com>
2020-12-19 01:41:02 +0300

    UI baby steps

--- Game/Game.js
+++ Game/Game.js
@@ -1,3 +1,26 @@
-import Ship from '../Ship/Ship'
-import Gameboard from '../Gameboard/Gameboard'
 
+import {userBoard, cpuBoard, filledDragBoard, filledCpuBoard} from '../Gameboard/Gameboard.js'
+
+const userBoardUI = document.querySelector('.userBoard')
+const cpuBoardUI = document.querySelector('.cpuBoard')
+const dragBoardUI = document.querySelector('.dragBoard')
+
+console.log("ben geldim")
+
+const boardUICreater = (boardToCreate, boardArray) => {
+    for (let i = 0 ; i < boardArray.length; i++) {  
+        const square = document.createElement('div') 
+        square.className = "square" 
+        square.id = i
+        if (boardArray[i] === "O") {    
+            square.classList.add("free")
+        } else if (boardArray[i] ) {
+            square.classList.add("taken")
+        }
+        boardToCreate.appendChild(square)
+    }
+}
+
+boardUICreater(userBoardUI, userBoard)
+boardUICreater(cpuBoardUI, cpuBoard)
+boardUICreater(dragBoardUI, filledDragBoard)
--- app.js
+++
@@ -1,3 +0,0 @@
-import Game from './Game/Game'
-
-Game()
