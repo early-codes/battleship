a73025c3f58178876be102410776d7ac26e4c131
ilker <lkrbalci@gmail.com>
2020-12-21 12:52:36 +0300

    find how to drag drop

--- Game/Game.js
+++ Game/Game.js
@@ -5,6 +5,16 @@ const userBoardUI = document.querySelector('.userBoard')
 const cpuBoardUI = document.querySelector('.cpuBoard')
 const dragBoardUI = document.querySelector('.dragBoard')
 
+const dragger = (event) => {
+    console.log(event);
+    console.log(event.srcElement.classList[2].toString())
+    const shipToDrag = document.querySelectorAll("." + event.srcElement.classList[2]);
+    console.log(shipToDrag);
+}
+
+const dropper = (event) => {
+    console.log(event, "dropper")
+}
 
 const boardUICreater = (boardToCreate, boardArray) => {
     for (let i = 0 ; i < boardArray.length; i++) {  
@@ -13,16 +23,23 @@ const boardUICreater = (boardToCreate, boardArray) => {
         square.id = i
         if (boardArray[i] === "O") {    
             square.classList.add("free")
+            if (i === 83) {
+                console.log(square.getRootNode(), square.parentElement, square.parentNode);
+            }
         } else if (boardArray[i] === "T") {
             square.classList.add("taken")
         } else if (boardArray[i].includes("+")) {
             square.classList.add("dragBoardTaken")
             square.classList.add(boardArray[i].split("+|")[0])
             square.classList.add(boardArray[i].split("+|")[1])
+            square.setAttribute("draggable", true)
+            square.addEventListener("dragstart", dragger)
         }
         boardToCreate.appendChild(square)
     }
 }
+
+
 
 boardUICreater(userBoardUI, userBoard)
 boardUICreater(cpuBoardUI, cpuBoard)
